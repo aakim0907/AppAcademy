4af65b40c47d70dbf10c1c6ea62641c16afda61e
Young Eun Kim <aakim0907@gmail.com>
2017-06-16 11:53:04 -0700

    Upload w3d5 projects

---
+++ w3d5/active_record_lite-i/lib/01_sql_object.rb
@@ -0,0 +1,91 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @columns = cols.first.map(&:to_sym)
+  end
+
+  def self.finalize!
+    columns.each do |column|
+      define_method(column) do
+        attributes[column]
+      end
+
+      define_method("#{column}=") do |val|
+        attributes[column] = val
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name || name.underscore.tableize
+  end
+
+  def self.all
+    DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+    SQL
+  end
+
+  def self.parse_all(results)
+    results.each do |result|
+      result.each do |attr_name, value|
+        p attr_name
+        p value
+      end
+    end
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
