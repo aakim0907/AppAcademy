3199d465ad4517eee7192cbefc899d34b9d2624e
Young Eun Kim <aakim0907@gmail.com>
2017-06-27 14:48:10 -0700

    Create class Session

--- w5d2/lib/controller_base.rb
+++ w5d2/lib/controller_base.rb
@@ -22,8 +22,10 @@ class ControllerBase
   # Set the response status code and header
   def redirect_to(url)
     raise "double render error" if already_built_response?
+
     @res.status = 302
     @res["Location"] = url
+
     @already_built_response = true
   end
 
@@ -32,8 +34,10 @@ class ControllerBase
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise "double render error" if already_built_response?
+
     @res.write(content)
     @res["Content-Type"] = content_type
+
     @already_built_response = true
   end
 
@@ -41,21 +45,19 @@ class ControllerBase
   # pass the rendered html to render_content
   def render(template_name)
     dir_path = File.dirname(__FILE__)
+
     template_fname = File.join(
-      dir_path, "..",
-      "views", self.class.name.underscore, "#{template_name}.html.erb"
-    )
+      dir_path, "..", "views",
+      self.class.name.underscore, "#{template_name}.html.erb")
 
     template_code = File.read(template_fname)
 
-    render_content(
-      ERB.new(template_code).result(binding),
-      "text/html"
-    )
+    render_content(ERB.new(template_code).result(binding), "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
