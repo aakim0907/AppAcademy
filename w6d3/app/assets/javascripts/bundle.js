542cb670a544a649dafa72020ceefb5d5cd14a25
Young Eun Kim <aakim0907@gmail.com>
2017-07-05 11:27:08 -0700

    Create followToggle class

--- w6d3/app/assets/javascripts/bundle.js
+++ w6d3/app/assets/javascripts/bundle.js
@@ -65,8 +65,68 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const FollowToggle = __webpack_require__(1)
+
+$(() => {
+  $("button.follow-toggle").each ( (idx, button) => new FollowToggle(button))
+});
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
+class FollowToggle {
+  constructor(el) {
+    this.$el = $(el);
+    this.userId = this.$el.data("user-id");
+    this.followState = this.$el.data("initial-follow-state");
+
+    this.render();
+    this.$el.on('click', this.handleClick.bind(this));
+  }
+
+  render() {
+    if (this.followState === "unfollowed") {
+      this.$el.text("Follow!");
+    } else {
+      this.$el.text("Unfollow!");
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    if (this.followState === "unfollowed") {
+      $.ajax({
+        method: "POST",
+        url: "/users/:id/follow",
+        dataType: "json",
+        data: this.$el,
+        success: () => {
+          this.followState = "followed";
+          this.render();
+        }
+      });
+    } else {
+      $.ajax({
+        method: "DELETE",
+        url: "/users/:id/follow",
+        dataType: "json",
+        data: this.$el,
+        success: () => {
+          this.followState = "unfollowed";
+          this.render();
+        }
+      });
+    }
+
+  }
+}
+
+module.exports = FollowToggle;
 
 
 /***/ })
