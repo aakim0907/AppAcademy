14492291a168361b2efe460774453b0170a119f3
Young Eun Kim <aakim0907@gmail.com>
2017-07-14 17:47:59 -0700

    Create SessionApiUtil

--- w7d5/BenchBnB/app/models/user.rb
+++ w7d5/BenchBnB/app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   end
 
   def reset_session_token!
-    self.session_token = SecureRandom.base64
+    self.session_token = SecureRandom.urlsafe_base64
     self.save
     self.session_token
   end
@@ -31,6 +31,6 @@ class User < ActiveRecord::Base
   private
 
   def ensure_session_token
-    self.session_token ||= SecureRandom.base64
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
 end
